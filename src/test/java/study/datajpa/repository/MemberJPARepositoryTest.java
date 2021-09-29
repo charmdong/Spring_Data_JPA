900ed93a1f7da30e08ddb80a3269d90b8d7a70a9
Donggun.Chung <chungdk1117@gmail.com>
2021-09-29 21:37:52 +0900

    :hammer: 2021.09.29.Wed [순수 JPA 구현]

--- src/test/java/study/datajpa/repository/MemberJPARepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJPARepositoryTest.java
@@ -1,6 +1,6 @@
 package study.datajpa.repository;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,7 +8,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 @SpringBootTest
 @Transactional
@@ -25,8 +25,35 @@ class MemberJPARepositoryTest {
 
         Member findMember = memberJPARepository.find(savedMember.getId());
 
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member);
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberJPARepository.save(member1);
+        memberJPARepository.save(member2);
+
+        Member findMember1 = memberJPARepository.findById(member1.getId()).get();
+        Member findMember2 = memberJPARepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        List<Member> members = memberJPARepository.findAll();
+        assertThat(members.size()).isEqualTo(2);
+
+        long count = memberJPARepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberJPARepository.delete(member1);
+        memberJPARepository.delete(member2);
+
+        long deleteCount = memberJPARepository.count();
+        assertThat(deleteCount).isEqualTo(0);
     }
 }
